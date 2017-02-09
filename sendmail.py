b1e75043080a2e02a8d61acef2e8d6d090e9e668
JayWall <sanjah.hall@gmail.com>
2017-02-09 00:21:52 -0500

    Exercise 1 completed

---
+++ sendmail.py
@@ -0,0 +1,24 @@
+import smtplib
+
+from_addr = 'a.lil.saint@gmail.com'
+to_addr = 'sanjah9@yahoo.com'
+
+message = """Hello
+"""
+from_name = 'SanJ'
+to_name = 'Jay'
+subject = 'testing'
+msg = message
+
+message_to_send = message.format(from_name, from_addr, to_name, to_addr, subject, msg)
+
+#Credentials (if needed)
+username = ''
+password = ''
+
+#The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username, password)
+server.sendmail(from_addr, to_addr, message_to_send)
+server.quit
